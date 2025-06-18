cc478ca00d5ce78e0bbc81a1f3d122df4ec5eb94
Oleh Bilynskyi <obslinskij@gmail.com>
2025-06-18 22:43:33 +0300

    Add MethodInfo and Author annotations

---
+++ src/app/MethodInfo.java
@@ -0,0 +1,14 @@
+package app;
+//Імпортуємо анотацію Retention, яка дозволяє вказати, як довго зберігається анотація
+import java.lang.annotation.Retention;
+//Імпортуємо enum RetentionPolicy для вибору політики збереження
+import java.lang.annotation.RetentionPolicy;
+
+//Вказуємо, що ця анотація буде зберігатись під час виконання
+@Retention(RetentionPolicy.RUNTIME)
+//Оголошення нової анотації з назвою MethodInfo
+public @interface MethodInfo {
+    String name(); //Оголошення елемента - назва методу
+    String returnType(); //Тип повернення методу
+    String description(); //Опис того, що робить метод
+}
