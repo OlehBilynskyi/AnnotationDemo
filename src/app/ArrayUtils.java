769c95a07d5a542d22ac30c0708b94301dbdc5f2
Oleh Bilynskyi <obslinskij@gmail.com>
2025-06-18 22:45:14 +0300

    Add ArrayUtils, Main class

---
+++ src/app/ArrayUtils.java
@@ -0,0 +1,52 @@
+package app;
+
+public class ArrayUtils {
+//Вказуємо анотацію MethodInfo з даними про метод findMax
+    @MethodInfo(
+            name = "findMax", //Назва методу
+            returnType = "int", //Метод повертає int
+            description = "Finds the maximum number in an array" //Опис дії методу
+    )
+    //Вказуємо автора методу
+    @Author(
+            firstName = "Oleh",
+            lastName = "Bilynskyi"
+    )
+    //Метод повертає найбільше число в масиві
+    public static int findMax(int[] array) {
+        //Ініціалізуємо змінну max першим елементом масиву
+        int max = array[0];
+        //Проходимо по кожному числу в масиві
+        for (int num : array) {
+            //Якщо поточне число більше max, оновлюємо max
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
+//Анотація з описом методу calculateAverage
+    @MethodInfo(
+            name = "calculateAverage", //Назва методу
+            returnType = "double", //Тип повернення - double
+            description = "Calculates the average of an array" //Опис
+    )
+
+    //Анотація з даними про автора
+    @Author(
+            firstName = "Ivan",
+            lastName = "Shevchenko"
+    )
+
+    //Метод обчислює середнє значення елементів масиву
+    public static double calculateAverage(int[] array) {
+        //Ініціалізуємо суму
+        int sum = 0;
+        //Проходимо по кожному числу і додаємо до суми
+        for (int num : array) {
+            sum += num;
+        }
+        //Ділимо суму на довжину масиву і повертаємо результат
+        return (double) sum / array.length;
+    }
+}
---
+++ src/app/Main.java
@@ -0,0 +1,39 @@
+package app;
+//Імпортуємо клас Method для роботи з методами через рефлексію
+import java.lang.reflect.Method;
+public class Main {
+    public static void main(String[] args) {
+        //Створюємо масив чисел
+        int[] numbers = {10, 20, 30, 40, 50};
+//Викликаємо метод finMax з класу ArrayUtils
+        int max = ArrayUtils.findMax(numbers);
+        //Викликаємо метод calculateAverage з класу ArrayUtils
+        double avg = ArrayUtils.calculateAverage(numbers);
+
+        //Виводимо результат в консоль
+        System.out.println("Max: " + max);
+        System.out.println("Average " + avg);
+        System.out.println();
+
+//Отримуємо клас ArrayUtils через рефлексію
+        Class<ArrayUtils> clas = ArrayUtils.class;
+        //Отримуємо всі методи класу
+        Method[] methods = clas.getDeclaredMethods();
+//Проходимо по кожному класу
+        for (Method method : methods) {
+            //Якщо метод має анотації MethodInfo та Author
+            if (method.isAnnotationPresent(MethodInfo.class) && method.isAnnotationPresent(Author.class)) {
+                MethodInfo methodInfo = method.getAnnotation(MethodInfo.class); //Отримуємо анотацію MethodInfo
+                Author author = method.getAnnotation(Author.class); //Отримуємо анотацію Author
+
+//Виводимо інформацію про метод
+                System.out.println("Method name: " + methodInfo.name());
+                System.out.println("Return type: " + methodInfo.returnType());
+                System.out.println("Description: " + methodInfo.description());
+                //Виводимо ім'я та прізвище автора
+                System.out.println("Author: " + author.firstName() + " " + author.lastName());
+                System.out.println("---------------------------");
+            }
+        }
+    }
+}
